3d4f62c90bb2764407e2790b7880429d79364249
allysongabrielx <gabriellol88@gmail.com>
2024-03-18 12:14:42 -0300

    comit pyautogui

---
+++ projeto-automacao/automacao.py
@@ -0,0 +1,11 @@
+import pyautogui
+from time import sleep
+
+# 1 - Clicar e digitar usuário
+# 2 - Clicar e digitar senha
+# 3 - Clicar em "Entrar"
+# 4 - Extrair cada produto
+#           4.1 - clicar e digitar produto 
+#           4.2 - clicar e digitar quantidade
+#           4.3 - clicar e digitar preço
+#           4.4 - clicar em registrar

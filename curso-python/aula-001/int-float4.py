778b9e6d72d8b2bc4e5c8dca9e014a6e63e14e5e
allysongabrielx <gabriellol88@gmail.com>
2024-03-27 14:57:13 -0300

    comit python

---
+++ curso-python/aula-001/int-float4.py
@@ -0,0 +1,20 @@
+"""
+Tipos de int e float
+int -> Número inteiro
+O tipo int representa qualquer número positivo ou negativo.
+int sem sinal é considerado positivo
+"""
+# Ex:
+print(11) #int
+print(-11) #int
+print(0) #int
+
+"""
+float -> Número com ponto flutuante
+O tipo float representa qualquer número positivo ou negativo com ponto flutuante.
+float sem sinal é considerado positivo.
+"""
+
+# EX:
+print(1.1, 10.11)
+print(0.0)
---
+++ curso-python/aula-001/strings3.py
@@ -0,0 +1,18 @@
+"""
+str -> strings -> texto
+Strings são textos que estão dentro de aspas (simples ou dupla)
+"""
+
+# Aspas simples:
+print('Allyson Gbariel')
+print(1,'Allyson "Gbariel"')
+
+
+# Aspas dupla:
+print("Allyson Gbariel")
+
+# Escape
+print("Allyson \"Gbariel\"")
+
+# r
+print(r"Allyson \"Gbariel\"")

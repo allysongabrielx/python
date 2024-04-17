a41db4209743b6ede78739be5568d50384903ebc
allysongabrielx <gabriellol88@gmail.com>
2024-04-17 15:24:45 -0300

    ex variável

---
+++ curso-python/aula-001/ex_variavel.py
@@ -0,0 +1,13 @@
+nome = 'Allyson Gabriel'
+sobrenome = 'Xavier de Lima'
+idade = 25
+ano_de_nascimento = 1998
+maior_de_idade = idade >= 18
+altura_metros = 1.70
+
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano de nascimento:', ano_de_nascimento)
+print('É maior de idade?', maior_de_idade)
+print('Altura em metros:', altura_metros)
--- curso-python/aula-001/variavel7.py
+++ curso-python/aula-001/variavel7.py
@@ -5,3 +5,11 @@ O sinal de = é o operador de atribuição.
 Ele é usado para atribuir um valor a um nome (variável).
 Uso: nome_variavel = expressão
 """
+nome = 'Allyson'
+idade = 25
+maior_de_idade = idade >= 18
+
+print('nome:', nome,)
+print('idade:', idade)
+print('É maior?', maior_de_idade)
+

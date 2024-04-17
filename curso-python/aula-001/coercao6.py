98e7e779de7b477ffb1660778308fe4fcb0dee6f
allysongabrielx <gabriellol88@gmail.com>
2024-04-17 10:31:18 -0300

    py

---
+++ curso-python/aula-001/coercao6.py
@@ -0,0 +1,10 @@
+"""
+conversão de tipos, coerção type convertion, typecasting,
+coercon é o ato de converter um tipo em outro tipo 
+imutáveis e primitivos:
+str, int , float, bool
+"""
+
+print( int('1'), type(int('1')))
+print(bool(' ')) # Espaço = True 
+print (str(11) + 'b') # str = para juntar tipos

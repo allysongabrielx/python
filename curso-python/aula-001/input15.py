158a2fab482136d7d44de6394253ec2afac664f5
allysongabrielx <gabriellol88@gmail.com>
2024-04-19 17:50:16 -0300

    comit 19/04/24

---
+++ curso-python/aula-001/input15.py
@@ -0,0 +1,15 @@
+#nome = input('Qual  seu nome?')
+
+#print(f'O seu nome é {nome}')
+
+numero_1 = int(input('Número 1:'))
+numero_2 = int(input('Número 2:'))
+
+print(f'A soma do número 1 com número 2 é: {numero_1 + numero_2}' )
+
+# OU
+
+int_numero_1 = int(numero_1)
+int_numero_2 = int(numero_2)
+
+print (f'A soma dos número é: {int_numero_1 + int_numero_2}')
--- curso-python/aula-001/tempCodeRunnerFile.py
+++ curso-python/aula-001/tempCodeRunnerFile.py
@@ -1,8 +1,1 @@
-resultados menores que 16 — magreza grave;
-resultados entre 16 e 16,9 — magreza moderada;
-resultados entre 17 e 18,5 — magreza leve;
-resultados entre 18,6 e 24,9 — peso ideal;
-resultados entre 25 e 29,9 — sobrepeso;
-resultados entre 30 e 34,9 — obesidade grau I;
-resultados entre 35 e 39,9 — obesidade grau II ou severa;
-resultados maiores do que 40 — obesidade grau III ou mórbida.
+resultado

9eb8919beb530944b535e65d34f337b82e56cb24
allysongabrielx <gabriellol88@gmail.com>
2024-04-20 10:50:37 -0300

    comit 20/04/24

--- curso-python/aula-001/input15.py
+++ curso-python/aula-001/input15.py
@@ -2,14 +2,32 @@
 
 #print(f'O seu nome é {nome}')
 
-numero_1 = int(input('Número 1:'))
-numero_2 = int(input('Número 2:'))
+#numero_1 = int(input('Número 1:'))
+#numero_2 = int(input('Número 2:'))
 
-print(f'A soma do número 1 com número 2 é: {numero_1 + numero_2}' )
+#print(f'A soma do número 1 com número 2 é: {numero_1 + numero_2}' )
 
 # OU
 
-int_numero_1 = int(numero_1)
-int_numero_2 = int(numero_2)
+#int_numero_1 = int(numero_1)
+#int_numero_2 = int(numero_2)
 
-print (f'A soma dos número é: {int_numero_1 + int_numero_2}')
+#print (f'A soma dos número é: {int_numero_1 + int_numero_2}')
+
+
+'''
+nome = input ('Digite seu nome: ')
+
+idade = input ('Digite sua idade: ')
+
+print (f'Olá, {nome}, você tem {idade} anos.')
+
+'''
+
+valor_1 = int(input ('Digite o primeiro valor: '))
+valor_2 = int(input ('Digite o segundo valor: '))
+valor_3 = int(input ('Digite o tercerio valor '))
+
+media = (valor_1 + valor_2 + valor_3) / 3
+
+print (f'A média dos três valores é: {media}')

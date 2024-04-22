38b90faad4218d0a644b93ba82d53af7b09e5132
allysongabrielx <gabriellol88@gmail.com>
2024-04-22 11:44:42 -0300

    comit 22/04/2024

--- curso-python/aula-001/ifelse16.py
+++ curso-python/aula-001/ifelse16.py
@@ -1,6 +1,8 @@
 # if / elif / else
 # se / se não se / se não
 """"
+# Exercício 1
+
 entrada = input ('Você quer "entrar" ou "sair"? ')
 
 
@@ -17,6 +19,9 @@ else:
 
 
 """
+"""
+
+# Exercício 2
 
 idade = int(input ('Informe sua idade: '))
 
@@ -30,3 +35,20 @@ elif idade >= 18 and idade < 65:
 else:
     print('Você é um idoso')
     print(' ')
+
+
+"""
+
+
+# Exercício 3
+
+numero = int(input('Digite um número:'))
+
+if numero > 0:
+    print ('Número positivo')
+
+elif numero < 0:
+    print ('Número negativo')
+
+else:
+    print ('Número zero')

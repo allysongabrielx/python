78b5a0fc2cc78c92df022b14e9054eb615a58b54
allysongabrielx <gabriellol88@gmail.com>
2024-04-23 21:13:32 -0300

    comit 23/04/2024

---
+++ curso-python/aula-001/ex_operadoresifelse.py
@@ -0,0 +1,10 @@
+primeiro_valor = input('Digite um valor: ')
+segundo_valor = input('Digite o segundo valor: ')
+
+if  primeiro_valor >= segundo_valor:
+
+    print (f'{primeiro_valor} é maior que ou igual {segundo_valor}')
+
+else:
+    print (f'{segundo_valor} é maior que {primeiro_valor}')
+

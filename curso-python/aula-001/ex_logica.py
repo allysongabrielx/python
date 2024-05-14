336d515aff15507cdc70fde98a97736bc6014c1e
allysongabrielx <gabriellol88@gmail.com>
2024-05-13 22:08:49 -0300

    comit 13/05/24

--- curso-python/aula-001/ex_logica.py
+++ curso-python/aula-001/ex_logica.py
@@ -3,19 +3,15 @@ Faça um programa que peça ao usuário para digitar um número inteiro,
 informe se este número é par ou ímpar. Caso o usuário não digite um número
 inteiro, informe que não é um número inteiro.
 """
+# try:
+#     numero = int(input ('Digite um número inteiro: '))
 
-numero = int(input ('Digite um número inteiro: '))
-
-
-if numero % 2 == 0:
-print(f'Caso o número {numero} for par')
-else:
-    print(f'Caso o número {numero} for impa')
-
-
-
-
-
+#     if numero % 2 == 0:
+#         print(f'O número {numero} é PAR')
+#     else:
+#         print(f'O número {numero} é IMPAR')
+# except ValueError:
+#     print('Isso não é um número inteiro.')
 
 
 """
@@ -24,8 +20,28 @@ descrito, exiba a saudação correspondente. Ex.
 Bom dia 0-11, Boa tarde 12-17 e Boa noite 18-23.
 """
 
+# horario = float(input('Digite a hora: '))
+
+# if horario >= 0 and horario < 12:
+#     print ('Bom dia!')
+# elif horario >= 12 and horario < 18:
+#     print ('Boa tarde!')
+# elif horario >= 18 and horario <= 23:
+#     print ('Boa noite!')
+# else:
+#     print('Horario invalido')
+    
+
 """
 Faça um programa que parte do primeiro nome do usuário. Se o nome tiver 4 letras ou
 menos escreva "Seu nome é curto"; se tiver entre 5 e 6 letras, escreva
 “Seu nome é normal”; maior que 6 escreva "Seu nome é muito grande".
 """
+primeiro_nome = input ('DIgite seu primeiro nome: ')
+
+if len (primeiro_nome) <= 4:
+    print('Seu nome é curto.')
+elif len (primeiro_nome) >= 5 and len(primeiro_nome) <= 6:
+    print('Seu nome é médio.')
+elif len (primeiro_nome) > 6:
+    print('Seu nome é grande.')
